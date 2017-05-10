40aa7f094d6c674a2aa437ff62698cd6608681b1
jfjhh <alex.striff1@gmail.com>
2017-05-09 23:42:35 -0700

    Math and Player Code!

--- math.js
+++ math.js
@@ -1,50 +1,115 @@
 /**
- * Perfect Ostrove Blossom Math.
+ * Perfect Ostrove Blossom Mathematics.
  * Alex Striff.
  */
 
-function Expression() /* RPN arguments. */
+"use strict";
+
+function Variable(name, value)
 {
-	this.op   = arguments.shift();
-	this.args = arguments;
+	this.name = name;
+	this.val  = value;
 }
 
-Expression.prototype.$ = function() {
-	if (this.op !== Object(this.op)) {
-		return this.op; /* Identity if primitive. */
-	} else {
-		return this.op(args);
-	}
+function generate_variable(name, value)
+{
+	return new Variable(name, value);
+}
+
+var Var = Variable;
+var _v  = generate_variable;
+
+function Expression(a) /* RPN arguments. */
+{
+	var args  = a;
+	this.op   = args.shift();
+	this.args = args;
+}
+
+function generate_expression() /* RPN arguments. */
+{
+	var args  = Array.from(arguments);
+	return new Expression(args);
+}
+
+var Exp = Expression;
+var _   = generate_expression;
+
+Expression.prototype.$ = function(a)
+{
+	return $(this, a);
 };
 
-var operators = [
-	add,
-	sub,
-	mult,
-	div,
-];
+function $(e, a) {
+	if (e.name !== undefined) {
+		for (var i = 0; i < a.length; i++) {
+			if (e.name === a[i].name) {
+				return a[i].val;
+			}
+		}
+		return "<" + e.name + "?>";
+	} else if (e.op === undefined) {
+	       return e; /* Identity if primitive. */
+	} else if (e.op !== Object(e.op)) {
+		return e.op; /* Identity if primitive. */
+	} else {
+		return e.op(e.args, a);
+	}
+};
 
 var PHI = (1 + Math.sqrt(5)) / 2;
 var PI  = Math.PI;
 var TAU = 2 * Math.PI;
 
-function un_argf(f)  { return function(x) {f(x[0]);} }
-function bin_argf(f) { return function(x) {f(x[0], x[1]);} }
+function mon_argf(f) { return function(a, v) {return f();}}
+function un_argf(f)  { return function(a, v) {return f($(a[0], v));}}
+function bin_argf(f) { return function(a, v) {return f($(a[0], v), $(a[1], v));}}
 
-function add(a)  { return a[0] + a[1]; }
-function sub(a)  { return a[0] - a[1]; }
-function mult(a) { return a[0] * a[1]; }
-function div(a)  { return a[0] / a[1]; }
-var pow = bin_argf(Math.pow);
+// function un_argf(f)  { return function(x)    { return f($(x)); }}
+// function bin_argf(f) { return function(x, y) { return f($(x), $(y)); }}
+
+function p_add(a, b) { return a + b; }
+function p_sub(a, b) { return a - b; }
+function p_mul(a, b) { return a * b; }
+function p_div(a, b) { return a / b; }
+
+var id    = un_argf(function(x){return x;});
+
+var add   = bin_argf(p_add);
+var sub   = bin_argf(p_sub);
+var mul   = bin_argf(p_mul);
+var div   = bin_argf(p_div);
+
+var pow   = bin_argf(Math.pow);
 
 var floor = bin_argf(Math.floor);
 var round = bin_argf(Math.round);
 var ceil  = bin_argf(Math.ceil);
 
-var sin  = un_argf(Math.sin);
-var cos  = un_argf(Math.cos);
-var tan  = un_argf(Math.tan);
-var asin = un_argf(Math.asin);
-var acos = un_argf(Math.acos);
-var atan = un_argf(Math.atan);
+var sin   = un_argf(Math.sin);
+var cos   = un_argf(Math.cos);
+var tan   = un_argf(Math.tan);
+var asin  = un_argf(Math.asin);
+var acos  = un_argf(Math.acos);
+var atan  = un_argf(Math.atan);
+
+var rand  = mon_argf(Math.random);
+
+var operators = [
+	add,
+	sub,
+	mul,
+	div,
+	pow,
+	floor,
+	round,
+	ceil,
+	sin,
+	cos,
+	tan,
+	asin,
+	acos,
+	atan,
+	rand,
+];
 
--- pob.js
+++ pob.js
@@ -3,28 +3,30 @@
  * Alex Striff.
  */
 
+"use strict";
+
 var WIDTH  = 3 * 100;
 var HEIGHT = 4 * 100;
 var FPS    = 120;
 var DELAY  = 1e3 / FPS;
 
 //var IWIDTH  = 4 * 50;
-var IWIDTH  = 12 * 50;
+var IWIDTH	= 12 * 50;
 var IHEIGHT = 3 * 50;
 
 var PHI    = (1 + Math.sqrt(5)) / 2;
 
-var canvas    = document.getElementById("bullet_canvas");
+var canvas	  = document.getElementById("bullet_canvas");
 canvas.width  = WIDTH;
 canvas.height = HEIGHT;
-var c         = canvas.getContext("2d");
+var c		  = canvas.getContext("2d");
 
 var icanvas    = document.getElementById("info_canvas");
 icanvas.width  = IWIDTH;
 icanvas.height = IHEIGHT;
-var info       = icanvas.getContext("2d");
+var info	   = icanvas.getContext("2d");
 
-var frames  = 0;
+var frames	= 0;
 var game_loop;
 
 var bullets = [];
@@ -34,12 +36,12 @@ var density_res  = IWIDTH;
 var density_data = [];
 var bullet_data  = [];
 
-var loops          = 0;
-var floops         = 0;
+var loops		   = 0;
+var floops		   = 0;
 var loop_timeouts  = [];
 var loop_kill_reqs = [];
 var loop_frames    = [];
-var scheduling     = false;
+var scheduling	   = false;
 
 function Bullet(x, y, t, v, r)
 {
@@ -51,11 +53,76 @@ function Bullet(x, y, t, v, r)
 	bullets.push(this);
 }
 
+function Player()
+{
+	this.x = WIDTH / 2;
+	this.y = HEIGHT * (PHI - 1);
+	this.r = 3;				/**< Hitbox radius. */
+	this.s = PHI;			/**< Speed. */
+	this.fs = 5;			/**< Focused speed ratio. */
+	this.l = 5;				/**< Lives. */
+	this.v = false;
+	this.iframe = false;
+	this.ul = undefined;
+	this.ll = undefined;
+	this.uu = true;
+	this.du = true;
+	this.lu = true;
+	this.ru = true;
+
+	this.color = "#F8F";
+}
+
+var player = new Player();
+
+function Danmaku(x, y, ti, tf, v, r, n, f)
+{
+	/* These are mostly functions of ti and time (frames). */
+	this.x	= x;  /**< x position of a bullet from ti. */
+	this.y	= y;  /**< y position of a bullet from ti. */
+	this.ti = ti; /**< Instantaneous angle function. Something like a phase shift. */
+	this.tf = tf; /**< Frame angle change function. How ti changes with increasing frames. */
+	this.v	= v;  /**< Velocity function. */
+	this.r	= r;  /**< Bullet radius function. TODO: Handle different bullet shapes?. */
+	this.n	= n;  /**< The number of bullets. Related to the density. */
+	this.f	= f;  /**< How frequent the entire pattern is. Functions will
+				likely be continuous, so this discretizes them. */
+}
+
+Danmaku.prototype.run = function(t) {
+	var time  = _v("t", t);
+	var n_val = this.n.$(args);
+	var ti_val = ti.$([t, _v("n", n_val)]);
+
+	var args = [t, _v("ti", ti_val)];
+
+	// var x  = this.x.$(args);
+	// var y  = this.y.$(args);
+	// var ti = this.ti.$(args);
+	// var tf = this.tf.$(args);
+	// var v  = this.v.$(args);
+	// var r  = this.r.$(args);
+
+	/**
+	 * Problem: Turn a function of the time and number into a function
+	 * dependent upon the number only, where the number is a function of the
+	 * time.
+	 *
+	 * TL;DR Implement partial application of functions?
+	 */
+	for (var i = 0; i < n; i++) {
+		var param = ti.$([t, _v("n", n_val)]);
+		new Bullet(x, y, tf, v, r);
+	}
+};
+
 function run_frames()
 {
 	for (var i = 0; i < floops; i++) {
 		if (frames % loop_frames[i].fmod === 0) {
-			loop_frames[i].func();
+			if (loop_frames[i].func() === -1) {
+				loop_frames[i].kill = true;
+			}
 		}
 	}
 }
@@ -113,7 +180,7 @@ function plot_density(density)
 	bullet_data.shift();
 	bullet_data.push(bullets.length);
 
-	var max = bmax = 0, min = 1, bmin = 31337;
+	var max = 0, bmax = 0, min = 1, bmin = 31337;
 	for (var i = 0; i < density_data.length; i++) {
 		var d = density_data[i];
 		var b = bullet_data[i];
@@ -213,17 +280,86 @@ function hausdorff(d)
 
 function update()
 {
+	// var hx_min = player.x - player.r;
+	// var hx_max = hx_min   + 2 * player.r;
+	// var hy_min = player.y - player.r;
+	// var hy_max = hy_min   + 2 * player.r;
+
+	// var hx_min = player.x + player.r / 3;
+	// var hx_max = hx_min   + player.r / 3;
+	// var hy_min = player.y + player.r / 3;
+	// var hy_max = hy_min   + player.r / 3;
+	
+	var hr = player.r;
+	var hx = player.x;
+	var hy = player.y;
+
+	var was_hit = false;
 	for (var i = 0; i < bullets.length; i++) {
 		var b = bullets[i];
 
 		//if (-WIDTH > b.x || b.x > 2*WIDTH || -HEIGHT > b.y || b.y > 2*HEIGHT) {
 		if (-WIDTH/2 > b.x || b.x > 3*WIDTH/2 || -HEIGHT/2 > b.y || b.y > 3*HEIGHT/2) {
 			bullets.splice(i, 1);
+			continue;
+		}
+
+		b.x += b.v * Math.cos(b.t);
+		b.y += b.v * Math.sin(b.t);
+
+        // var sx = bullets[i].x + bullets[i].r;
+        // var sy = bullets[i].y + bullets[i].r;
+
+		var sx = bullets[i].x;
+		var sy = bullets[i].y;
+		var sr = bullets[i].r;
+
+        var sx_min = bullets[i].x - bullets[i].r;
+        var sy_min = bullets[i].y - bullets[i].r;
+        var sx_max = bullets[i].x + 2 * bullets[i].r;
+        var sy_max = bullets[i].y + 2 * bullets[i].r;
+
+		// if (!player.iframe
+		//if (hx_min <= sx && sx <= hx_max && hy_min <= sy && sy <= hy_max) {
+		//if (sx_min <= hx && hx <= sx_max && sy_min <= hy && hy <= sy_max) {
+		if (Math.sqrt(Math.pow(sx - hx, 2) + Math.pow(sy - hy, 2)) < sr + hr) {
+			player.l--;
+			//if (player.l <= 0) {
+			//die();
+			//} else {
+			//player.iframe = true;
+			was_hit = true;
+			c.fillColor = "#000";
+			c.strokeColor = "#0F0";
+			c.strokeRect(sx_min, sy_min, sx_max, sy_max);
+			//player.color  = "#F00";
+			/*
+					schedule_frames(function() {
+						player.iframe = false;
+						player.color  = "#F8F";
+						return -1;
+					}, 10);
+					*/
+			hit();
+			//}
+		}
+
+		if (was_hit) {
+			player.color = "#F00";
 		} else {
-			b.x += b.v * Math.cos(b.t);
-			b.y += b.v * Math.sin(b.t);
+			player.color = "#F8F";
 		}
 	}
+}
+
+function die()
+{
+	console.log("You died!");
+}
+
+function hit()
+{
+	console.log("You were hit!");
 }
 
 function spiral(x, y, d, t, v, r)
@@ -231,27 +367,45 @@ function spiral(x, y, d, t, v, r)
 	new Bullet(x, y, t, v, r);
 }
 
+/*
 function circle(x, y, d, p, v, r)
 {
 	for (var t = 0; t < 2*Math.PI; t += 2*Math.PI/d) {
 		new Bullet(x, y, t + p, v, r);
 	}
 }
+*/
+
+function circle(x, y, d, p, v, r)
+{
+	for (var t = 0; t < 2*Math.PI; t += 2*Math.PI/d) {
+		var angle = _(add, t, p);
+		new Bullet(x, y, angle.$(), v, r);
+	}
+}
 
 function asteroid(d, v, r)
 {
+	var argv = v;
 	for (var t = 0; t < 2*Math.PI; t += 2*Math.PI/d) {
-		var x = (WIDTH  / 2) + WIDTH*Math.pow(Math.cos(t), 3);
-		var y = (HEIGHT / 2) + WIDTH*Math.pow(Math.sin(t), 3);
-		new Bullet(x, y, Math.PI + t, v, r);
+		var x = _(add, _(div, WIDTH, 2), _(mul, WIDTH, _(pow, _(cos, _v("t")), 3)));
+		var y = _(add, _(div, HEIGHT, 2), _(mul, WIDTH, _(pow, _(sin, _v("t")), 3)));
+		var v = _(add, 1, _(mul, 0.01, _(rand)));
+		var v = _(id, _v("v"));
+		var vs = [_v("t", t), _v("v", argv)];
+		new Bullet(x.$(vs), y.$(vs), Math.PI + t, v.$(vs), r);
 	}
 }
 
 function render()
 {
+	/* Clear BG. */
 	c.fillStyle = "#000";
 	c.fillRect(0, 0, WIDTH, HEIGHT);
 
+	/* Bullets. */
+	c.fillStyle = "#F00";
+	c.fillRect(WIDTH/2 - 1, HEIGHT/2 - 1, 2, 2);
 	for (var i = 0; i < bullets.length; i++) {
 		var b = bullets[i];
 		if (0 < b.x && b.x < WIDTH && 0 < b.y && b.y < HEIGHT) {
@@ -263,7 +417,68 @@ function render()
 		}
 	}
 
+	/* Player. */
+	c.fillStyle = c.strokeStyle = player.color;
+	// c.fillRect(player.x, player.y, 2 * player.r, 2 * player.r);
+	c.beginPath();
+	c.arc(player.x, player.y, player.r, 0, 2 * Math.PI, true);
+	c.closePath();
+	c.fill();
+
+	/* Info. */
 	plot_density(density);
+}
+
+function input()
+{
+  var dx, dy;
+  
+  if (!(player.uu || player.du)) {
+    if (player.ul) {
+      dy = -1;
+    } else {
+      dy = 1;
+    }
+  } else if (!(player.uu)) {
+    dy = -1;
+  } else if (!(player.du)){
+    dy = 1;
+  } else {
+    dy = 0;
+  }
+  
+  if (!(player.lu || player.ru)) {
+    if (player.ll) {
+      dx = -1;
+    } else {
+      dx = 1;
+    }
+  } else if (!(player.lu)) {
+    dx = -1;
+  } else if (!(player.ru)){
+    dx = 1;
+  } else {
+    dx = 0;
+  }
+  
+  var xf = player.x + (player.s * dx);
+  var yf = player.y + (player.s * dy);
+  var hw = player.r;
+  
+  if (xf < 0) {
+    xf = 1;
+  } else if (xf > 320 - hw) {
+    xf = 320 - hw - 1; 
+  }
+  
+  if (yf < 0) {
+    yf = 1;
+  } else if (yf > 450 - hw) {
+    yf = 450 - hw - 1; 
+  }
+  
+  player.x = xf;
+  player.y = yf;
 }
 
 function init()
@@ -275,6 +490,7 @@ function init()
 
 	game_loop = schedule_loop(function() {
 		run_frames();
+		input();
 		update();
 		render();
 		frames++;
@@ -291,15 +507,92 @@ function init()
 	pa = pb = 0;
 	d = 64;
 
-	schedule_frames(function(){asteroid(200, 1, 2);}, 3.75*FPS, true);
+	//schedule_frames(function(){asteroid(200, 1, 2);}, 3.75*FPS, true);
 
 	//schedule_frames(function(){spiral(WIDTH/2, 0, d, 2*Math.PI*PHI*pb++/d, 0.1, 2);}, 1);
 
-	// schedule_frames(function(){circle(0,       0,        d, 2*Math.PI*PHI*pb++/d, 0.1, 2);}, 0.5*FPS);
-	// schedule_frames(function(){circle(WIDTH,   0,        d, 2*Math.PI*PHI*pb++/d, 0.1, 2);}, 0.5*FPS);
-	// schedule_frames(function(){circle(0,       HEIGHT,   d, 2*Math.PI*PHI*pb++/d, 0.1, 2);}, 0.5*FPS);
-	// schedule_frames(function(){circle(WIDTH,   HEIGHT,   d, 2*Math.PI*PHI*pb++/d, 0.1, 2);}, 0.5*FPS);
+	schedule_frames(function(){circle(WIDTH / 2, HEIGHT / 5, d, 2*Math.PI*PHI*pb++/d, 0.25, 3);}, 0.75*FPS);
 }
 
 init();
+
+window.onkeydown = function(event)
+{
+  switch (event.keyCode) {
+    case 38: /* Up. */
+    case 87: /* W.  */
+      if (player.uu) {
+        player.uu = false;
+        player.ul = true;
+      }
+      break;
+    case 40: /* Down. */
+    case 83: /* S.    */
+      if (player.du) {
+        player.du = false;
+        player.ul = false;
+      }
+      break;
+    case 37: /* Left. */
+    case 65: /* A.    */
+      if (player.lu) {
+        player.lu = false;
+        player.ll = true;
+      }
+      break;
+    case 39: /* Right. */
+    case 68: /* D.     */
+      if (player.ru) {
+        player.ru = false;
+        player.ll = false;
+      }
+      break;
+    case 16: /* Shift. */
+      if (!(player.v)) {
+        player.s /= player.fs;
+        player.v  = true;
+        // setImageURL("player", "ostrove_hitbox.png");
+      }
+      break;
+    case 27: /* ESC. */
+    case 81: /* Q.   */
+      stop_game(true);
+      break;
+    case 82: /* R.   */
+      die();
+      break;
+    default:
+      // console.log(event.keyCode);
+      break;
+  }
+};
+
+window.onkeyup = function()
+{
+  switch (event.keyCode) {
+    case 38: /* Up. */
+    case 87: /* W.  */
+      player.uu = true;
+      break;
+    case 40: /* Down. */
+    case 83: /* S.    */
+      player.du = true;
+      break;
+    case 37: /* Left. */
+    case 65: /* A.    */
+      player.lu = true;
+      break;
+    case 39: /* Right. */
+    case 68: /* D.     */
+      player.ru = true;
+      break;
+    case 16: /* Shift. */
+      player.s *= player.fs;
+      player.v  = false;
+      // setImageURL("player", "ostrove_head.png");
+      break;
+    default:
+      break;
+  }
+};
 
